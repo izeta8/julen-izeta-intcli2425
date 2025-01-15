9b63ed2e13d4fb1fcb011e112db817e12fab1225
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-01-15 13:51:23 +0100

    update tailwind conf to correct styles.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
-import {nextui} from '@nextui-org/theme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(slider|popover).js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [nextui()],
+  plugins: [],
 }
