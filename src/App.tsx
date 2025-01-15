28e4845137880de5526ae8d60ab12c6fa2b46bf2
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-01-15 13:39:29 +0100

    fix errors to be able to deploy to vercel.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css'
 import PotionItem from './components/PotionItem'
 import { potions } from './data/data'
 import { Potion } from './types/Potion'
-import EffectFilter from './components/EffectFilter'
 import Button from './components/Button'
+import EffectFilter from './components/EffectFilter'
+import LevelFilter from './components/LevelFilter'
+
 
 function App() {
 
@@ -16,6 +18,10 @@ function App() {
     console.log(secondaryEffectText);
   }, [secondaryEffectText]); 
 
+  const handleFilter = () => {
+    
+  }
+
   return (
       <>
         <div className={`bg-gray-900 border-2 border-[#cda882] mb-3 p-3 gap-3 flex`}>
@@ -24,9 +30,12 @@ function App() {
             setSecondaryEffectText={setSecondaryEffectText}
           />
 
+          <LevelFilter />
+
           <Button 
             label='Filter' 
-            />
+            onClick={handleFilter}
+          />
         </div>
         <div className='grid grid-cols-2 gap-3'>
           {displayedPotions.map((potion, index) => {
--- src/components/EffectFilter.tsx
+++ src/components/EffectFilter.tsx
@@ -1,18 +1,17 @@
 
-import { SyntheticEvent } from "react";
-
 interface EffectFilterProps {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   setSecondaryEffectText: Function;
 }
 
 const EffectFilter: React.FC<EffectFilterProps> = ({setSecondaryEffectText}) => {
 
-  const handleInputChange = (e: SyntheticEvent) => {
-    setSecondaryEffectText(e.target.value);
+  const handleInputChange = (inputContent: string) => {
+    setSecondaryEffectText(inputContent);
   }
   
   return (
-    <input type="text" placeholder="Filter by secondary effect" className="p-2 w-52" onChange={handleInputChange} />
+    <input type="text" placeholder="Filter by secondary effect" className="p-2 w-52" onChange={(e) => handleInputChange((e.target as HTMLInputElement).value)} />
   )
 
 }
