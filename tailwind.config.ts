f035f148a612c0bc8d18f3118b81917811b94dc5
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-01-15 13:27:08 +0100

    Implmenet spinner from nextui.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
+import {nextui} from '@nextui-org/theme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(slider|popover).js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 }
